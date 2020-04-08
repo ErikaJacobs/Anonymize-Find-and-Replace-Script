cbfa2f8c347652aa25b1ad50d5c3a757cf970f10
Erika Jacobs <erikaashley3@gmail.com>
2020-04-08 11:27:36 -0700

    Create Find and Replace.py

---
+++ Find and Replace.py
@@ -0,0 +1,23 @@
+# The purpose of this Python script is to find and replace text in 
+# multiple text-based files without having to open them and physically
+# find and replace. While simple, this code can be very powerful!
+
+# The files that need words replaced
+files = ["C:/Users/cluel/Desktop/ErikaJacobs_PythonToSQL_PG.py", 
+         "C:/Users/cluel/Desktop/ErikaJacobs_PythonToSQL_PG2.py"]
+
+# The words being replaced
+# Example: "Cat" is before, "turtle" is after
+words = [('Cat', 'turtle'), ('meow', 'moo'), ('dog', 'turnip')]
+
+# Loop through all files and words
+for file in files:
+    for before, after in words:
+        fin = open(file, "rt")
+        data = fin.read()
+        data = data.replace(before, after)
+        fin.close()
+
+        fin = open(file, "wt")
+        fin.write(data)
+        fin.close()
